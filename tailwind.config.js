20b3a780cca7c0eb94c7331ffff8377f55ccdde2
= <=>
2024-04-02 21:02:09 +0100

    feat: config and tailwind customization

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3366FF',
+        black: '#171717',
+        'black-light': '#242428',
+        light: '#FAFCFD',
+        'dark-blue': '#0D2159'
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
+

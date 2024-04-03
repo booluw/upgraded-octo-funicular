f73c33f19f532d87e7f89aee254694d1c805c151
= <=>
2024-04-03 17:42:31 +0100

    fix: homepage fixes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 import AppButton from '@/components/AppButton.vue'
 import AppCard from '@/components/AppCard.vue'
 import AppInput from '@/components/AppInput.vue'
+
+const router = useRouter()
 
 const reviews = reactive([
   {
@@ -57,6 +60,18 @@ const reviews = reactive([
     condition: 'traffic'
   }
 ])
+
+const query = ref('')
+const error = ref(false)
+
+const search = function () {
+  error.value = false
+  if (query.value === '') {
+    error.value = true
+    return
+  }
+  router.push(`/areas?query=${query.value}`)
+}
 </script>
 
 <template>
@@ -69,32 +84,41 @@ const reviews = reactive([
         See through the lenses of people who have lived or visited the neighbourhood you might have
         in mind.
       </p>
-      <AppInput type="search" placeholder="Enter Address" class="mb-[8px] md:mb-[20px]">
-        <template #icon>
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            class="fill-none text-[#0D2159] dark:text-[#BACAF5]"
-          >
-            <path
-              d="M7.4074 12.1482C10.0256 12.1482 12.1481 10.0257 12.1481 7.40749C12.1481 4.78925 10.0256 2.66675 7.4074 2.66675C4.78916 2.66675 2.66666 4.78925 2.66666 7.40749C2.66666 10.0257 4.78916 12.1482 7.4074 12.1482Z"
-              stroke="currentColor"
-              stroke-width="1.18519"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-            <path
-              d="M13.333 13.3331L10.7552 10.7554"
-              stroke="currentColor"
-              stroke-width="1.18519"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </template>
-      </AppInput>
-      <AppButton type="primary" class="uppercase">Search</AppButton>
+      <form @submit.prevent="search">
+        <div class="text-sm text-red-500 mb-[2px] md:mb-[5px]" v-if="error">Please add a search query</div>
+        <AppInput
+          v-model="query"
+          type="search"
+          placeholder="Enter Address"
+          class="mb-[8px] md:mb-[20px]"
+          required
+        >
+          <template #icon>
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              class="fill-none text-[#0D2159] dark:text-[#BACAF5]"
+            >
+              <path
+                d="M7.4074 12.1482C10.0256 12.1482 12.1481 10.0257 12.1481 7.40749C12.1481 4.78925 10.0256 2.66675 7.4074 2.66675C4.78916 2.66675 2.66666 4.78925 2.66666 7.40749C2.66666 10.0257 4.78916 12.1482 7.4074 12.1482Z"
+                stroke="currentColor"
+                stroke-width="1.18519"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M13.333 13.3331L10.7552 10.7554"
+                stroke="currentColor"
+                stroke-width="1.18519"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </template>
+        </AppInput>
+        <AppButton type="primary" class="uppercase">Search</AppButton>
+      </form>
     </div>
     <div class="hidden h-full md:flex gap-[20px] overflow-hidden relative">
       <div

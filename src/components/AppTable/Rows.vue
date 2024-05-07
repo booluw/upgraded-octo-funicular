fc0192794173468be366f01b8a55512083ba025c
= <=>
2024-05-07 12:35:37 +0100

    feat: admin dashboard

---
+++ src/components/AppTable/Rows.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const emits = defineEmits(['action'])
+const props = defineProps<{
+  row: object
+  action?: string[]
+}>()
+</script>
+<template>
+  <div class="border-b border-primary-light flex">
+    <div class="" v-for="(item, index) in row" :key="index">
+      {{ item }}
+    </div>
+  </div>
+</template>
